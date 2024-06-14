f9e11fed1b30176be613f933c8529bdae7d13bef
José Paiva <josecarlospaivasantos2001@gmail.com>
2024-06-14 15:37:28 -0300

    .

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Iphone iphone = new Iphone();
+        iphone.ligar();
+        iphone.tocar();
+        iphone.adicionarNovaAba();
+    }
+}
---
+++ src/Musica.java
@@ -0,0 +1,5 @@
+public interface Musica {
+    public void tocar();
+    public void pausar();
+    public void selecionarMusica();
+}
